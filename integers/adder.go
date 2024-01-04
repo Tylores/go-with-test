a88884832bc2cb64d5ee6be14b67e3ec973cba2a
Tylores <tylor.slay@gmail.com>
2024-01-04 12:56:52 -0800

    the integers test was easy, but the doc generation is old and now should use go doc command instead of godoc. I didn't really get it working correcty thought.

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Add is a method that adds go figure
+func Add(x, y int) int {
+	return x + y
+}
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,9 +1,18 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
 
 func TestAddr(t *testing.T) {
-	sum := Add()
+	sum := Add(2, 2)
 	expected := 4
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
